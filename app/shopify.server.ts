33a602fdaef60a72ba8370e55b2e5c10bfc5dbb1
Tomoya-take <tt.manager.t@gmail.com>
2025-08-19 15:57:57 +0900

    Update shopify.server.ts

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -1,12 +1,14 @@
 import { shopifyApp } from "@shopify/shopify-app-remix/server";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+import type { SessionStorage } from "@shopify/shopify-api";
+
+const sessionStorage: SessionStorage = new SQLiteSessionStorage("./database.sqlite");
 
 export const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY!,
   apiSecretKey: process.env.SHOPIFY_API_SECRET!,
   scopes: process.env.SCOPES?.split(",") || [],
   appUrl: process.env.SHOPIFY_APP_URL!,
-  sessionStorage: new SQLiteSessionStorage("./database.sqlite"),
+  sessionStorage,
 });
 
-export const authenticate = shopify.authenticate;
