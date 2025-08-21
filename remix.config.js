2bf231b8ce9b77c8f0461975639baf60990ea387
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 03:41:26 +0900

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,17 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  serverModuleFormat: "cjs",
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "cjs", // or esm に統一
   serverDependenciesToBundle: [
     /^@shopify\/shopify-app-remix/,
     /^@shopify\/polaris/,
     /^@shopify\/polaris-icons/,
   ],
+  future: {
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true,
+  },
 };
