955c47950ae9d946670f92fb82a280f4b44c6f35
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 22:44:25 +0900

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,7 @@
 // app/entry.server.tsx
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
+import shopify from "./shopify.server";
 
 export default function handleRequest(
   request: Request,
@@ -8,6 +9,9 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: any
 ) {
+  // ✅ 管理画面 iframe 対応のヘッダを自動付与（超重要）
+  shopify.addDocumentResponseHeaders(request, responseHeaders);
+
   const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
@@ -19,3 +23,5 @@ export default function handleRequest(
     headers: responseHeaders,
   });
 }
+
+
