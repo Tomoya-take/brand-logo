3d2c5b9dd71108400c4792f2414dddfb027144f8
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 15:07:47 +0900

    Update auth.exit-iframe.tsx

--- app/routes/auth.exit-iframe.tsx
+++ app/routes/auth.exit-iframe.tsx
@@ -1,16 +1,9 @@
-// app/routes/api.auth.exit-iframe.tsx
-import { redirect } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
+// app/routes/auth.exit-iframe.tsx
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import shopify from "../shopify.server";
 
-export async function loader({ request }: { request: Request }) {
-  // iframe内から来るので、authにリダイレクトして再ログイン処理
-  const url = new URL(request.url);
-  const shop = url.searchParams.get("shop");
-
-  const redirectUrl = new URL("/api/auth", request.url);
-  if (shop) {
-    redirectUrl.searchParams.set("shop", shop);
-  }
-
-  return redirect(redirectUrl.toString());
+export async function loader({ request }: LoaderFunctionArgs) {
+  return shopify.authenticate.exitIframe(request);
 }
+
+
