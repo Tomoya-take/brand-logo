1b0f6ddc2a88783869aa91c8a7f59eb00de65480
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 13:32:56 +0900

    Update api.auth.tsx

--- app/routes/api.auth.tsx
+++ app/routes/api.auth.tsx
@@ -1,5 +1,5 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
-import { shopify } from "~/shopify.server";
+import shopify from "../shopify.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   // これが Response を返す（リダイレクト含む）
