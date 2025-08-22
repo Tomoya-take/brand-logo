355e96a149385468ca453682748b6ec96b1bb507
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 15:58:02 +0900

    Delete app/routes/api.auth.tsx

--- app/routes/api.auth.tsx
+++
@@ -1,8 +0,0 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import shopify from "../shopify.server";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  // これが Response を返す（リダイレクト含む）
-  return shopify.authenticate.admin(request);
-}
-
