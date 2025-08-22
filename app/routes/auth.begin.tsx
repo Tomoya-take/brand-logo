b9c28c69d1b9e3d89b6ef766fbd6a3301e68caf0
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 22:18:03 +0900

    Update auth.begin.tsx

--- app/routes/auth.begin.tsx
+++ app/routes/auth.begin.tsx
@@ -1,14 +1,10 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import shopify from "../shopify.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  console.log("DEBUG: reached /auth/begin loader");
-
-  return shopify.auth.begin({
-    shop: new URL(request.url).searchParams.get("shop")!,
-    callbackPath: "/auth/callback",
-    isOnline: true,
-    rawRequest: request,
-  });
+  // OAuth 開始
+  return shopify.auth.begin(request);
 }
+
+
 
