2d04c634f58284ccfe3b32428488feb508311718
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 15:53:25 +0900

    Update auth.tsx

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,8 +1,9 @@
-// app/routes/auth.tsx
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { shopify } from "../shopify.server";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
+export async function loader({ request }: LoaderFunctionArgs) {
   return shopify.authenticate.admin(request);
-};
+}
+
+
 
