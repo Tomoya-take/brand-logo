3f1d74e817c8cf26410016ae07ddccf8b6f82bd0
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 12:02:17 +0900

    Update entry.client.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,8 @@
 // app/entry.client.tsx
-import "./styles/polaris.css";
+import "@shopify/polaris/build/esm/styles.css";
 import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
 
 hydrateRoot(document, <RemixBrowser />);
+
 
