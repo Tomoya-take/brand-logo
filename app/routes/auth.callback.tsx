12b5f68c768d15ad208af74e59a0830658c3a20d
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 15:13:31 +0900

    Update auth.callback.tsx

--- app/routes/auth.callback.tsx
+++ app/routes/auth.callback.tsx
@@ -1,8 +1,9 @@
+// app/routes/auth.callback.tsx
 import { LoaderFunctionArgs } from "@remix-run/node";
 import shopify from "../shopify.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  // これも Response を返す（認証成功時はセッション保存、リダイレクト含む）
-  return shopify.authenticate.admin(request);
+  // コールバック用の処理を必ず使う
+  return shopify.authenticate.callback(request);
 }
 
