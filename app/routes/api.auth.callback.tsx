d99b4dd0f36b4c4a4bf322038fad60f5d3d53bf3
Tomoya-take <tt.manager.t@gmail.com>
2025-08-22 15:58:14 +0900

    Delete app/routes/api.auth.callback.tsx

--- app/routes/api.auth.callback.tsx
+++
@@ -1,8 +0,0 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import shopify from "../shopify.server";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  // これも Response を返す（認証成功時はセッション保存、リダイレクト含む）
-  return shopify.authenticate.admin(request);
-}
-
